c699d4a11f598b84aa6aaecafbc172048017a5db
Qodesmith <theqodesmith@gmail.com>
2025-09-04 17:50:48 -0400

    Added comments to build file for CPUs

--- build.ts
+++ build.ts
@@ -1,7 +1,9 @@
 await Bun.build({
   entrypoints: ['./index.ts'],
   compile: {
+    // Using the "baseline" version to support the older CPU powering Unraid.
     target: 'bun-linux-x64-baseline-musl',
+    // target: 'bun-linux-x64-musl', // For newer CPUs.
     outfile: './unraidFileServer',
   },
   env: 'inline',
